8b8f482c607205c989c300421006bc8db3ce84ea
Emma Hogan <em30hoga@siena.edu>
2020-11-17 18:00:15 -0500

    SQLTable creates public instance variable  array of SQLColumns

--- SQLTable.java
+++ SQLTable.java
@@ -10,7 +10,8 @@ public class SQLTable {
 
     public String name;     // Name of the table
     public int num_cols;    // Number of columns in the table
-    public String[] fields;     // Names of the columns
+    public SQLColumn[] columns;     // Columns
+    private String[] column_strs;
 
     /**
      Construct a new Waypoint.
@@ -19,8 +20,13 @@ public class SQLTable {
      @param num_cols Number of columns
      @param field_str The fields with their types as written in the SQL create table statement
      */
-    public SQLTable(String name, int num_cols, String field_str) {
-
+    public SQLTable(String name, String field_str) {
+        this.name = name;
+        column_strs = field_str.split(", ");
+        num_cols = column_strs.length;
+        for(int i=0; i<num_cols; i++){
+            columns[i] = new SQLColumn(column_strs[i]);
+        }
     }
 
 
